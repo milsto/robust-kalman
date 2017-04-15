1ed5b935549a13ba018f87a0a988cfe4553afd3c
Milos Stojanovic <milos-stojanovic@outlook.com>
2017-04-15 12:22:43 +0200

    Track variable history in more clean way
    Track variable history for plotting using a dictionary abstraction.

--- example.py
+++ example.py
@@ -22,6 +22,20 @@ matplotlib.rcParams.update(params)
 # plt.plot(t, hv(t))
 # plt.show()
 
+
+class VariablesHistory:
+    def __init__(self):
+        self._variables_history = dict()
+
+    def __getitem__(self, item):
+        return self._variables_history[item]
+
+    def update(self, variable_name, value):
+        if variable_name not in self._variables_history:
+            self._variables_history[variable_name] = list()
+
+        self._variables_history[variable_name].append(value)
+
 # Example 1
 # dt = 4.0
 # end_time = 400.0
@@ -32,7 +46,7 @@ matplotlib.rcParams.update(params)
 # P0 = np.eye(3, dtype=np.float32) * 0.01
 # sigma_process = 1.0
 # sigma_measure = 1.0
-x0_kalman = np.array([[0, 0]], np.float32).T
+#x0_kalman = np.array([[0, 0]], np.float32).T
 
 
 # Example 2
@@ -57,31 +71,28 @@ kalman_robust = RobustKalman(F, [0], H, x0_kalman, P0, Q0, R0, use_robust_estima
 
 x = x0
 z = np.matmul(H, x0)
-x_history = list()
-xres_history = list()
-z_history = list()
-x_kalman_history = list()
-inov_history = list()
-cee_x_history = list()
-cee_xres_history = list()
 cee_x = 0.0
 cee_xres = 0.0
 step = 2
 t_axis = np.arange(0, end_time, dt)
+
+history = VariablesHistory()
+
 for t in t_axis:
-    x_history.append(x)
-    z_history.append(z)
-    x_kalman_history.append(kalman_linear.x)
-    xres_history.append(kalman_robust.x)
+    history.update('x', x)
+    history.update('z', z)
+    history.update('x_kalman', kalman_linear.x)
+    history.update('x_kalman_robust', kalman_robust.x)
     cee_x += (np.linalg.norm(kalman_linear.x - x) / (np.linalg.norm(x) + 0.0001)) / step
     cee_xres += (np.linalg.norm(kalman_robust.x - x) / (np.linalg.norm(x) + 0.0001)) / step
-    cee_x_history.append(cee_x)
-    cee_xres_history.append(cee_xres)
+    history.update('cee_x_history', cee_x)
+    history.update('cee_xres_history', cee_xres)
 
     q = np.random.normal(0.0, sigma_process, size=(1, 1))
     #r = 0.85 * np.random.normal(0.0, sigma_measure, size=(1, 1)) + 0.15 * np.random.normal(0.0, 5.0, size=(1, 1))
     rare_event = 1 if np.random.uniform(0, 1.0) > 0.9 else 0
     r = np.random.normal(0.0, sigma_measure, size=(1, 1)) + np.random.choice([-1.0, 1.0]) * np.random.uniform(1.0, 2.0) * rare_event #+ 0.15 * np.random.normal(0.0, 5.0, size=(1, 1))
+
     x = np.matmul(F, x) + np.matmul(G, q)
     z = np.matmul(H, x) + r
 
@@ -90,30 +101,30 @@ for t in t_axis:
     kalman_robust.time_update()
     kalman_robust.measurement_update(z)
 
-    inov_history.append(kalman_robust.inovation)
+    history.update('inov', kalman_robust.inovation)
 
     step += 1
 
 plt.figure()
-plt.plot(t_axis, [x[0, 0] for x in x_history], 'g', label='x0')
+plt.plot(t_axis, [x[0, 0] for x in history['x']], 'g', label='x0')
 
 plt.hold(True)
 
-plt.plot(t_axis, [z[0, 0] for z in z_history], 'b', label='z0')
+plt.plot(t_axis, [z[0, 0] for z in history['z']], 'b', label='z0')
 
-plt.plot(t_axis, [k[0, 0] for k in x_kalman_history], 'r', label='x est')
+plt.plot(t_axis, [k[0, 0] for k in history['x_kalman']], 'r', label='x est')
 
-plt.plot(t_axis, [k[0, 0] for k in xres_history], 'k', label='x est robust')
+plt.plot(t_axis, [k[0, 0] for k in history['x_kalman_robust']], 'k', label='x est robust')
 
 plt.legend()
 
 plt.figure()
-plt.plot(t_axis, cee_x_history / np.arange(1, len(cee_x_history) + 1, 1), 'r')
+plt.plot(t_axis, history['cee_x_history'] / np.arange(1, len(history['cee_x_history']) + 1, 1), 'r')
 plt.hold(True)
-plt.plot(t_axis, cee_xres_history / np.arange(1, len(cee_xres_history) + 1), 'k')
+plt.plot(t_axis, history['cee_xres_history'] / np.arange(1, len(history['cee_xres_history']) + 1), 'k')
 
 plt.figure()
-plt.plot(t_axis, [k[0, 0] for k in inov_history], 'k')
+plt.plot(t_axis, [k[0, 0] for k in history['inov']], 'k')
 plt.title('inovation')
 
 plt.show()
