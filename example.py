c211d80b3d5208dc04a5aa1f7637f5b4fb0a0563
Milos Stojanovic <milos-stojanovic@outlook.com>
2017-04-15 11:40:22 +0200

    Add example to test the estimators. Working code, but WIP

---
+++ example.py
@@ -0,0 +1,119 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+from robust_kalman import RobustKalman, HuberScore
+
+params = {
+   'axes.labelsize': 10,
+   'font.size': 10,
+   'legend.fontsize': 10,
+   'xtick.labelsize': 10,
+   'ytick.labelsize': 10,
+   'text.usetex': True,
+   'font.family': 'Times'
+}
+matplotlib.rcParams.update(params)
+
+# Plot robust score function
+# t = np.linspace(-50, 50, 1000)
+# h = HuberScore()
+# hv = np.vectorize(h.evaluate)
+# plt.plot(t, hv(t))
+# plt.show()
+
+# Example 1
+# dt = 4.0
+# end_time = 400.0
+# F = np.array([[1, dt, dt**2 / 2], [0, 1, dt], [0, 0, 1]], np.float32)
+# G = np.array([[0, 0, 1]], np.float32).T
+# H = np.array([[1, 0, 0]], np.float32)
+# x0 = np.array([[0, 0, 0]], np.float32).T
+# P0 = np.eye(3, dtype=np.float32) * 0.01
+# sigma_process = 1.0
+# sigma_measure = 1.0
+x0_kalman = np.array([[0, 0]], np.float32).T
+
+
+# Example 2
+dt = 0.01
+end_time = 1.0
+F = np.array([[1, dt], [0, 1]], np.float32)
+G = np.array([[0.5 * dt**2, dt]], np.float32).T
+H = np.array([[1, 0]], np.float32)
+x0 = np.array([[0.1, 0.1]], np.float32).T
+P0 = np.ones((2, 2), np.float32) * 0.001
+sigma_process = 8.0
+sigma_measure = 0.1
+x0_kalman = np.array([[0, 0]], np.float32).T
+
+
+Q0 = np.matmul(G, G.T) * sigma_process**2
+R0 = np.eye(1, dtype=np.float32) * sigma_measure**2
+
+kalman_linear = RobustKalman(F, [0], H, x0_kalman, P0, Q0, R0, use_robust_estimation=False)
+kalman_robust = RobustKalman(F, [0], H, x0_kalman, P0, Q0, R0, use_robust_estimation=True)
+
+
+x = x0
+z = np.matmul(H, x0)
+x_history = list()
+xres_history = list()
+z_history = list()
+x_kalman_history = list()
+inov_history = list()
+cee_x_history = list()
+cee_xres_history = list()
+cee_x = 0.0
+cee_xres = 0.0
+step = 2
+t_axis = np.arange(0, end_time, dt)
+for t in t_axis:
+    x_history.append(x)
+    z_history.append(z)
+    x_kalman_history.append(kalman_linear.x)
+    xres_history.append(kalman_robust.x)
+    cee_x += (np.linalg.norm(kalman_linear.x - x) / (np.linalg.norm(x) + 0.0001)) / step
+    cee_xres += (np.linalg.norm(kalman_robust.x - x) / (np.linalg.norm(x) + 0.0001)) / step
+    cee_x_history.append(cee_x)
+    cee_xres_history.append(cee_xres)
+
+    q = np.random.normal(0.0, sigma_process, size=(1, 1))
+    #r = 0.85 * np.random.normal(0.0, sigma_measure, size=(1, 1)) + 0.15 * np.random.normal(0.0, 5.0, size=(1, 1))
+    rare_event = 1 if np.random.uniform(0, 1.0) > 0.9 else 0
+    r = np.random.normal(0.0, sigma_measure, size=(1, 1)) + np.random.choice([-1.0, 1.0]) * np.random.uniform(1.0, 2.0) * rare_event #+ 0.15 * np.random.normal(0.0, 5.0, size=(1, 1))
+    x = np.matmul(F, x) + np.matmul(G, q)
+    z = np.matmul(H, x) + r
+
+    kalman_linear.time_update()
+    kalman_linear.measurement_update(z)
+    kalman_robust.time_update()
+    kalman_robust.measurement_update(z)
+
+    inov_history.append(kalman_robust.inovation)
+
+    step += 1
+
+plt.figure()
+plt.plot(t_axis, [x[0, 0] for x in x_history], 'g', label='x0')
+
+plt.hold(True)
+
+plt.plot(t_axis, [z[0, 0] for z in z_history], 'b', label='z0')
+
+plt.plot(t_axis, [k[0, 0] for k in x_kalman_history], 'r', label='x est')
+
+plt.plot(t_axis, [k[0, 0] for k in xres_history], 'k', label='x est robust')
+
+plt.legend()
+
+plt.figure()
+plt.plot(t_axis, cee_x_history / np.arange(1, len(cee_x_history) + 1, 1), 'r')
+plt.hold(True)
+plt.plot(t_axis, cee_xres_history / np.arange(1, len(cee_xres_history) + 1), 'k')
+
+plt.figure()
+plt.plot(t_axis, [k[0, 0] for k in inov_history], 'k')
+plt.title('inovation')
+
+plt.show()
